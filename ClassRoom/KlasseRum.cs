ec42afeab663fd71f60950593326f265bf3a5ccd
Nikolaj Dyring <nikodyring@gmail.com>
2018-01-29 10:53:37 +0100

    Opgave 2.
    KlasseRum.cs er tilføjet.  Få instance fields og en constructor.

---
+++ ClassRoom/KlasseRum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassRoom
+{
+    public class KlasseRum
+    {
+        private string _className;
+        private List<Studerende> _klasseListe;
+        private DateTime semesterStart;
+
+        public KlasseRum()
+        {
+                
+        }
+    }
+}
