1224c3eddb2b9f586782a76d5f09e52b3b8a8002
Nikolaj Dyring <nikodyring@gmail.com>
2018-01-29 10:52:56 +0100

    Opgave 1
    Har tilføjet en "Studerende.cs" klasse, og tilføjet nogle properties og en constructor med parametre.

---
+++ ClassRoom/Studerende.cs
@@ -0,0 +1,28 @@
+﻿namespace ClassRoom
+{
+    public class Studerende
+    {
+        private string _name;
+        private int _birthMonth;
+        private int _birthDay;
+
+        public string Name => _name;
+        public int BirthMonth => _birthMonth;
+        public int BirthDay => _birthDay;
+
+
+        public Studerende(string name, int birthMonth, int birthDay)
+        {
+            name = Name;
+            birthMonth = BirthMonth;
+            birthDay = BirthDay;
+        }
+
+
+
+
+    }
+
+
+
+}
